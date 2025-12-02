437d1ac5f8bc8a70254e5da8aafb11fe54865ee6
jieyingong <jieying.gong@mail.utoronto.ca>
2025-12-01 19:35:08 -0500

    add extra handling for opp null case and edge attack

--- RoboSoccer_EV3/src/roboAI.h
+++ RoboSoccer_EV3/src/roboAI.h
@@ -274,8 +274,10 @@ double compute_opp_angle_diff_to_target(struct RoboAI *ai, double target_x,
                                         double target_y);
 double compute_opp_distance_to_target(struct RoboAI *ai, double target_cx,
                                       double target_cy);
+bool check_target_validation(struct RoboAI *ai, double target_cx,
+                             double target_cy);
 
-// soccer defense helper
+ // soccer defense helper
 void compute_defense_target(struct RoboAI *ai, double *target_cx,
                             double *target_cy);
 
@@ -291,7 +293,7 @@ int compute_ball_nearest_edge(struct RoboAI *ai, double *edge_x,
 
 // general utilities
 bool check_anything_lost(struct RoboAI *ai);
-
+bool check_ball_self_lost(struct RoboAI *ai);
 // correction helper
 double correct_motion_vector(double *smx, double *smy, double rotate_angle_deg);
 
